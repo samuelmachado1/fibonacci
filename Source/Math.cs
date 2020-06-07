814361e2a4037e9e617ece49e6bef0dbc697964a
Samuel Estrella <samuelmachado1@gmail.com>
2020-06-06 23:21:23 -0300

    Download basic project files

---
+++ Source/Math.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace Codenation.Challenge
+{
+    public class Math
+    {
+        public List<int> Fibonacci()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsFibonacci(int numberToTest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
