2bb606c948c72294be14bc7b929e14625b29144b
Samuel Estrella <samuelmachado1@gmail.com>
2020-06-06 23:36:39 -0300

    Implementing Fibonacci method

--- Source/Math.cs
+++ Source/Math.cs
@@ -7,7 +7,19 @@ namespace Codenation.Challenge
     {
         public List<int> Fibonacci()
         {
-            throw new NotImplementedException();
+            // The "limit" variable contains the maximum value to be displayed for the fibonacci sequence.
+            int limit = 350;
+            List<int> fibonacciList = new List<int>();
+
+            // Startup => cont, store = 0, preNumber = 1; 
+            // Condition => store < limit; 
+            // Update => cont = preNumber, preNumber = store, store = cont + preNumber
+            for (int cont, store = 0, preNumber = 1; store < limit; cont = preNumber, preNumber = store, store = cont + preNumber)
+            {
+                fibonacciList.Add(store);
+            }
+
+            return fibonacciList;
         }
 
         public bool IsFibonacci(int numberToTest)
